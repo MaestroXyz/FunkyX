bc0f3c38368bbcc2b74492eec571e29be1f167c1
Maestro <95646935+MaestroXyz@users.noreply.github.com>
2021-12-16 21:15:58 +0700

    Update Funky.py

--- Funky.py
+++ Funky.py
@@ -13,5 +13,5 @@ Pacotes = [
  codecs.decode('081efd40', 'hex_codec'),
  codecs.decode('081e7eda', 'hex_codec')]
 print ' TOLS DDOS ATTACK BY FUNKY
-print ' attack By Funky to ip %s port %s                                                               ' % (orgip, port)
+print ' attack By Funky to ip %s port %s              ' % (orgip, port)
 print ' Easy Down'
